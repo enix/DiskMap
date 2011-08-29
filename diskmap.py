46e33abc63f51ec852b4a3696da84584889dafa0
Sebastien Wacquiez <sebastien.wacquiez@sipartech.com>
2011-08-29 20:36:30 +0200

    Rev++

--- diskmap.py
+++ diskmap.py
@@ -52,6 +52,20 @@ class StorageManager(object):
                 m = m.groupdict()
                 m["controller"] = ctrl
                 self.enclosures[m["logicalid"]] = m
+            for m in re.finditer("Device is a Hard disk\n +"
+                                 "Enclosure # +: (?P<enclosure>[^ ]+)\n +"
+                                 "Slot # +: (?P<slot>[^ ]+)\n +"
+                                 "State +: (?P<state>[^ ]+)\n +"
+                                 "Size .in MB./.in sectors. +: (?P<sizemb>[^/]+)/(?P<sizesector>[^ ]+)\n +"
+                                 "Manufacturer +: (?P<manufacturer>[^ ]+)\n +"
+                                 "Model Number +: (?P<model>[^ ]+)\n +"
+                                 "Firmware Revision +: (?P<firmware>[^ ]+)\n +"
+                                 "Serial No +: (?P<serial>[^ ]+)\n +"
+                                 "Protocol +: (?P<protocol>[^ ]+)\n +"
+                                 "Drive Type +: (?P<drivetype>[^ ]+)\n", tmp):
+                print m.groupdict()
+            
+                                
 
     def discover(self):
         """ use sas2ircu to populate controller, enclosures ands disks """
