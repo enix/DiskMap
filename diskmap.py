2b4592affbe7852cf067decb9116080a6738c864
Sebastien Wacquiez <sebastien.wacquiez@sipartech.com>
2011-08-29 19:20:10 +0200

    Rev++

--- diskmap.py
+++ diskmap.py
@@ -34,9 +34,9 @@ class StorageManager(object):
         tmp = re.findall("(\n +[0-9]+ +.*)", tmp)
         for ctrl in tmp:
             m = re.match(" +(?P<index>[0-9]) +(?P<adaptertype>[^ ].*[^ ]) +(?P<vendorid>[^ ]+) +"
-                         "(?P<deviceid>[^ ]+) +(?P<pciadress>[^ ]*:[^ ]*) +(?P<subsysvenid>[^ ]+)"
-                         "+(?P<subsysdevid>[^ ]+) *", ctrl)
-            print m
+                         "(?P<deviceid>[^ ]+) +(?P<pciadress>[^ ]*:[^ ]*) +(?P<subsysvenid>[^ ]+) +"
+                         "(?P<subsysdevid>[^ ]+) *", ctrl)
+
             if m:
                 ctrl = m.groupdict()
                 ctrl["index"] = int(ctrl["index"])
