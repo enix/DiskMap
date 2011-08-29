afe5d3cf7f896c4520cd3a53a3618191a3a73fc6
Sebastien Wacquiez <sebastien.wacquiez@sipartech.com>
2011-08-29 20:38:53 +0200

    Rev++

--- diskmap.py
+++ diskmap.py
@@ -53,7 +53,7 @@ class StorageManager(object):
                 m["controller"] = ctrl
                 self.enclosures[m["logicalid"]] = m
             for m in re.finditer("Device is a Hard disk\n +"
-#                                 "Enclosure # +: (?P<enclosure>[^ ]+)\n +"
+                                 "Enclosure # +: (?P<enclosure>[^ ]+)\n +"
 #                                 "Slot # +: (?P<slot>[^ ]+)\n +"
 #                                 "State +: (?P<state>[^ ]+)\n +"
 #                                 "Size .in MB./.in sectors. +: (?P<sizemb>[^/]+)/(?P<sizesector>[^ ]+)\n +"
