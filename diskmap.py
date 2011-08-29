79f0b4dc7e434df746a79358d15e4bbbcaa7de9f
Sebastien Wacquiez <sebastien.wacquiez@sipartech.com>
2011-08-29 20:19:15 +0200

    Rev++

--- diskmap.py
+++ diskmap.py
@@ -9,6 +9,9 @@ def run(cmd, *args):
 
 sas2ircu = "/usr/sbin/sas2ircu"
 
+class Disk(object):
+    def __init__(self):
+        pass
 
 class Enclosure(object):
     def __init__(self):
@@ -17,19 +20,13 @@ class Enclosure(object):
         self.disks[disk.id] = disk
 
 
-class Disk(object):
-    def __init__(self):
-        pass
-
-
 class StorageManager(object):
     def __init__(self):
         self.enclosures = {}
         self.controller = {}
 
-    def populate(self):
-        """ use sas2ircu to populate controller, enclosures ands disks """
-        # First, get Ctrl
+    def discover_controller(self):
+        """ Discover controller present in the computer """
         tmp = run(sas2ircu, "LIST")
         tmp = re.findall("(\n +[0-9]+ +.*)", tmp)
         for ctrl in tmp:
@@ -42,6 +39,24 @@ class StorageManager(object):
                 ctrl["index"] = int(ctrl["index"])
                 self.controller[ctrl["index"]] = ctrl
 
+    def discover_enclosure(self, *ctrls):
+        """ Discover enclosure wired to controller. If no controller specified, discover them all """
+        if not ctrls:
+            ctrls = self.controller.indexes()
+        for ctrl in ctrls:
+            tmp = run(sas2ircu, ctrl, "DISPLAY")
+            for m in re.finditer("Enclosure# +: (?P<enclosureid>[^ ]+)\n +"
+                                 "Logical ID +: (?P<logicalid>[^ ]+)\n +"
+                                 "Numslots +: (?P<numslot>[0-9]+)", tmp):
+                m = m.groupdict()
+                print m
+                self.enclosure[m["logicalid"]] = m
+
+    def discover(self):
+        """ use sas2ircu to populate controller, enclosures ands disks """
+        self.discover_controller()
+        self.discover_enclosure()
+        
     def __str__(self):
         from pprint import pformat
         result = [ "Controller" ]
@@ -57,7 +72,7 @@ if __name__ == "__main__":
     if not os.path.isfile(sas2ircu):
         sys.exit("Error, cannot find sas2ircu (%s)"%sas2ircu)
     st = StorageManager()
-    st.populate()
+    st.discover()
     print st
     
     
