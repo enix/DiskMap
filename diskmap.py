da2d5e02581ab8b20c925bf5b5aa0d0d404318cd
Sebastien Wacquiez <sebastien.wacquiez@sipartech.com>
2011-08-29 19:19:13 +0200

    Rev++

--- diskmap.py
+++ diskmap.py
@@ -32,6 +32,7 @@ class StorageManager(object):
         # First, get Ctrl
         tmp = run(sas2ircu, "LIST")
         tmp = re.findall("(\n +[0-9]+ +.*)", tmp)
+        print tmp
         for ctrl in tmp:
             m = re.match(" +(?P<index>[0-9]) +(?P<adaptertype>[^ ].*[^ ]) +(?P<vendorid>[^ ]+) +"
                          "(?P<deviceid>[^ ]+) +(?P<pciadress>[^ ]*:[^ ]*) +(?P<subsysvenid>[^ ]+)"
