9febdcad14e91e512a863c8eefbbb3e31b432c9a
Sebastien Wacquiez <sebastien.wacquiez@sipartech.com>
2011-08-30 02:21:18 +0200

    Rev++

--- diskmap.py
+++ diskmap.py
@@ -176,6 +176,10 @@ if __name__ == "__main__":
     #if not os.path.isfile(sas2ircu):
     #    sys.exit("Error, cannot find sas2ircu (%s)"%sas2ircu)
     sm = SesManager()
-    sm.cmdloop()
+    if len(sys.argv) > 1:
+        sm.precmd("")
+        sm.onecmd(" ".join(sys.argv[1:]))
+    else:
+        sm.cmdloop()
     
     
