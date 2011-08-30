aca8e00f29b442f31d1b1ac4c36de137ed97fe3f
Sebastien Wacquiez <sebastien.wacquiez@sipartech.com>
2011-08-30 04:13:15 +0200

    Rev++

--- diskmap.py
+++ diskmap.py
@@ -280,8 +280,7 @@ class SesManager(cmd.Cmd):
             self.do_save()
 
     def complete_alias(self, text, line, begidx, endidx):
-        print repr(line)
-        if line.startswith("-r "):
+        if line.startswith("alias -r "):
             return [ i for i in self.aliases.keys() if i.startswith(line) ]
         if line.count(" ") >= 2:
             return [ i for i in self.complete_ledon(text, line, begidx, endidx)
