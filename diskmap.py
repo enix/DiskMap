012db1268f3f935c4bd85260d131d8646b3960af
Sebastien Wacquiez <sebastien.wacquiez@sipartech.com>
2011-08-30 05:05:11 +0200

    Rev++

--- diskmap.py
+++ diskmap.py
@@ -225,6 +225,7 @@ class SesManager(cmd.Cmd):
             else:
                 c, e, s = tmp
                 c, e, s = long(c), long(e), long(s)
+                print c, e, s
                 return [ disk for disk in self.disks.values()
                          if disk["controller"] == c and disk["enclosureindex"] == e
                          and s == None or disk["slot"] == s ]
