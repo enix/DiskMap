472e35b815f8e82520d3fa3c37ba29cb95ae0794
Sebastien Wacquiez <sebastien.wacquiez@sipartech.com>
2011-09-01 19:32:05 +0200

    Rev++

--- diskmap.py
+++ diskmap.py
@@ -141,7 +141,7 @@ class SesManager(cmd.Cmd):
                 #for disk in disks:
                 #    print disk
                 for disk in re.finditer(" +(?P<name>[^ ]+) +(?P<state>[^ ]+) +"
-                                        "(?P<read>[^ ]+) +(?P<write>[^ ]+) +(?P<cksum>[^ ]+) +"):
+                                        "(?P<read>[^ ]+) +(?P<write>[^ ]+) +(?P<cksum>[^ ]+) +", m["config"]):
                     disk = disk.groupdict()
                     if disk["name"] == "NAME": continue
                     print disk
