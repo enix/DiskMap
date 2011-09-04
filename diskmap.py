4a40e52fd40e45f9cea2c9b32880e7cfa615d727
Sebastien Wacquiez <sebastien.wacquiez@sipartech.com>
2011-09-04 20:49:41 +0200

    Rev++

--- diskmap.py
+++ diskmap.py
@@ -141,6 +141,7 @@ class SesManager(cmd.Cmd):
                                         "(?P<read>[^ \t]+) +(?P<write>[^ \t]+) +"
                                         "(?P<cksum>[^\n]+))?\n", m["config"]):
                     disk = disk.groupdict()
+                    print disk
                     if not disk["name"]: continue
                     if disk["name"][-4:-2] == "d0":
                         disk["name"] = disk["name"][:-2]
